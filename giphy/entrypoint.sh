ae864ace342bb5666c1c6561154a719f60ca9592
John Hashem III <jhashem3@yahoo.com>
2024-12-14 16:40:43 -0500

    new action for giphy

---
+++ giphy/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+GITHUB_TOKEN=$1
+GIPHY_API_KEY=$2
+
+pPKwlT9v3c2gl6QdiO6lfvVc9QUPpPQU
+
+#Get PR Num
+pull_request_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+echo PR Number - $pull_request_number
+
+#Random GIF from Giphy API
+giphy_response=$(curl -s "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_API_KEY&tag=&rating=g")
+echo Giphy Response - $giphy_response
+
+gif_url=$(echo "$giphy_response") | jq --raw-output .data.images.downsized.url)
+echo GIPHY_URL - $gif_url
+
+comment_response=$(curl -sX POST -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github.v3+json" \
+  -d "{\"body\": \"### PR - #$pull_request_number. \n ### 🎉 Thank you for this contribution! \n  ![GIF]($gif_url) \"}" \
+  "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$pull_request_number/comments")
+
+# Extract and print the comment URL from the comment response
+comment_url=$(echo "$comment_response" | jq --raw-output .html_url)
