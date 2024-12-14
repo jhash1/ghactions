c7504aa60557f89ecaa38725f3ffb44701a54731
John Hashem III <jhashem3@yahoo.com>
2024-12-14 17:30:52 -0500

    refined entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
+# Get the GitHub Token and Giphy API Key from GitHub Action inputs
 GITHUB_TOKEN=$1
 GIPHY_API_KEY=$2
 
-
-#Get PR Num
-pull_request_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+=pull_request_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 echo PR Number - $pull_request_number
 
-#Random GIF from Giphy API
-giphy_response=$(curl -s "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_API_KEY&tag=&rating=g")
+giphy_response=$(curl -s "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_API_KEY&tag=thank%20you&rating=g")
 echo Giphy Response - $giphy_response
 
-gif_url=$(echo "$giphy_response") | jq --raw-output .data.images.downsized.url
+gif_url=$(echo "$giphy_response" | jq --raw-output .data.images.downsized.url)
 echo GIPHY_URL - $gif_url
 
 comment_response=$(curl -sX POST -H "Authorization: token $GITHUB_TOKEN" \
@@ -22,4 +20,3 @@ comment_response=$(curl -sX POST -H "Authorization: token $GITHUB_TOKEN" \
 
 # Extract and print the comment URL from the comment response
 comment_url=$(echo "$comment_response" | jq --raw-output .html_url)
-
